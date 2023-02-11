ed54935616c485e7d8ee3d7dc300e48c39c5d626
Nikola Dukić <nd20201014@student.fon.bg.ac.rs>
2023-02-12 00:50:17 +0100

    Poslednje izmene

--- routes/api.php
+++ routes/api.php
@@ -49,3 +49,9 @@ Route::group(['middleware' => ['auth:sanctum']], function () {
 Route::post('/register',[AuthController::class,'register']);
 
 Route::post('/login',[AuthController::class,'login']);
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/kategorijas',[KategorijaController::class,'index']);
